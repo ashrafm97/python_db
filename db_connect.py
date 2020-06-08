dd8b02bb2b9249cca3623d864b3074576a231572
Ashraf <ashrafmohamud@gmail.com>
2020-06-08 15:11:24 +0100

    commit for test

--- db_connect.py
+++ db_connect.py
@@ -1,1 +1,16 @@
-import pyodbcgit 
+import pyodbc
+
+
+# connecting_to_python = pyodbc.connect('driver= {OBDC driver for SQL}; Server = databases2.spartaglobal.academy;')
+
+server = "databases2.spartaglobal.academy"
+database = "Northwind"
+username = "SA"
+password = "Passw0rd2018"
+
+
+connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+print(connection)
+
+
